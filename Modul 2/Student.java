2316c75bafab68ee4af4d16850ab06b519b2310c
DestaZzz <destasatrio222@student.ub.ac.id>
2026-03-24 16:41:33 +0700

    Add files via upload

---
+++ Modul 2/Student.java
@@ -0,0 +1,28 @@
+public class Student {
+    private String nama;
+    private int skor;
+
+    public Student(String nama) {
+        this.nama = nama;
+    }
+
+    private void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void ubahNama(String nama) {
+        setNama(nama);
+    }
+
+     public static void setNama(Student std, String nama) { 
+        std.nama = nama;
+    }
+
+    public String getNama() {
+         return this.nama;
+    }
+
+    public int getSkor() { 
+        return this.skor;
+    }
+}
