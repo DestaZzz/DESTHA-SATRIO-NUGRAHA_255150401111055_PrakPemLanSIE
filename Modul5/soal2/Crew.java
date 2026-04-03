2807d0330cfd0569be50167cd6d3dddb7db37bb1
DestaZzz <destasatrio222@student.ub.ac.id>
2026-04-04 01:27:06 +0700

    Add files via upload

---
+++ Modul5/soal2/Crew.java
@@ -0,0 +1,38 @@
+public class Crew implements ICrew, Kickable { 
+ private String name; 
+ private boolean isAlive = true;
+
+ public Crew(String name) { 
+    
+ this.name = name; 
+ } 
+  
+ @Override 
+ public void doWork() { 
+ System.out.println("Crew " + this.name + " is doing work."); 
+ } 
+
+ @Override 
+ public String getName() { 
+ return this.name; 
+ } 
+
+ public void callMeeting() { 
+    if (isAlive) {
+ System.out.print(this.name + " found a corpse "); 
+ System.out.print("and calls a meeting. "); 
+ System.out.println("Let's find the impostor!"); 
+ } else {
+ System.out.println(this.name + " is dead and cannot call a meeting.");
+ }
+}
+
+ public void setAlive(boolean status) {
+    this.isAlive = status;
+ }
+ @Override
+ public void kick() {
+ System.out.println(this.name + " was kicked from the spaceship!");
+}
+ 
+}
---
+++ Modul5/soal2/IImpostor.java
@@ -0,0 +1,4 @@
+public interface IImpostor { 
+ void kill(ICrew crew); 
+}
+
